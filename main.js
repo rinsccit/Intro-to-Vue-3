48396a747e81fb01bc2825488ac6c9e641eb1661
Rahul Sood <w0510619@campus.nscc.ca>
2025-04-02 15:28:21 -0400

    Completed the lesson 4 of the tutorial

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp ({
         return {
             product: 'Socks',
             description: 'Description: They are available in two colours and are woolen socks.',
-            image: './assets/images/sockd_green.jpg',
+            image: './assets/images/socks_green.jpg',
         }
     }
 })
