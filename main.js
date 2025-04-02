dfb3fa1d1ee9bcd0cd14747349d64c1c89a82728
Rahul Sood <w0510619@campus.nscc.ca>
2025-04-02 12:55:55 -0400

    Second commit after completing the coding challenge

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp ({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Description: They are available in two colours and are woolen socks.'
+        }
+    }
+})
